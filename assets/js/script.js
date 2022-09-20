af454bb69475b513c3afa9d2680f4a32e331ebe6
Michel Maia <programador.michel.maia@gmail.com>
2022-09-20 13:14:06 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,17 @@ menuToggle.addEventListener("click", () => {
     show = !show;
 })
 
+
+// esconder ou mostrar uma div 
+var btn = document.querySelector('#show-or-hide');
+var container = document.querySelector('.container');
+
+btn.addEventListener('click',function() {
+    if(container.style.display === 'block') {
+        container.style.display = 'none';
+
+    } else {
+        container.style.display = 'block';
+    }
+});
+
